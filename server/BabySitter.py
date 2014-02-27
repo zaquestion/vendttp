7497e1944273366157115792c79ab1d987aea7c4
Zachary Wiedmann <zaqthefreshman@gmail.com>
2014-02-27 12:59:16 -0500

    Update BabySitter.py
    added a delay before starting the server. To give the server a chance to fully close

--- server/BabySitter.py
+++ server/BabySitter.py
@@ -1,7 +1,9 @@
 import subprocess
+import time
 
 print("BabySitter is sitting the baby")
 restartOnExit = 1
 while 0 != restartOnExit:
     print("Baby fell over, re-sitting")
+    time.sleep(5)
     restartOnExit = subprocess.call("python server.py")
